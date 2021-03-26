95a64126665ced18274f54bba31b075088a0413d
Twigonometry <mac.j.goodwin@gmail.com>
2021-03-26 15:48:28 +0000

    Add expiry date to token

--- hack_the_box/cereal/gentoken.py
+++ hack_the_box/cereal/gentoken.py
@@ -1,9 +1,10 @@
 import jwt
+from datetime import datetime, timedelta
 
 #take key from old git code - commit ID 8f2a1a88f15b9109e1f63e4e4551727bfb38eee5
 key = "secretlhfIH&FY*#oysuflkhskjfhefesf"
 
 #encode with HMAC-SHA-256
-encoded = jwt.encode({"some": "payload"}, key, algorithm="HS256")
+encoded = jwt.encode({"exp": datetime.utcnow() + timedelta(days=7)}, key, algorithm="HS256")
 
 print(encoded)
