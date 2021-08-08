cf18c609efe2ea2ffed68728092c3dab05b88e9d
Twigonometry <mac.j.goodwin@gmail.com>
2021-08-08 15:40:21 +0100

    SMTP Username Enum script

---
+++ scripts/SMTP-Username-Enum/vrfy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import socket
+import sys
+if len(sys.argv) != 3:
+ print("Usage: vrfy.py <target IP> <username list>")
+ sys.exit(0)
+# Create a Socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to the Server
+connect = s.connect((sys.argv[1],25))
+# Receive the banner
+banner = s.recv(1024)
+print(banner)
+# Open file
+f = open(sys.argv[2],'r')
+# VRFY a user
+for user in f.readlines():
+  s.send(('VRFY ' + user).encode('utf-8'))
+  result = s.recv(1024)
+  print(result)
+# Close the socket
+s.close()
